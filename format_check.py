ac78feb1ca2fee75a53a5a2debdb42cd50adc257
sergioSEa <sergio_andreu@hotmail.es>
2017-03-29 12:53:09 +0200

    Includes new modalities

--- format_check.py
+++ format_check.py
@@ -19,13 +19,16 @@ parser.add_argument('-ann_file', action="store",default = 'n/p', dest = 'ann_fil
 parser.add_argument('-sim_mut', action="store",default = 'n/p', dest = 'sim_mut')
 parser.add_argument('-sim_recsel', action="store", default = 'n/p', dest = 'sim_recsel')
 parser.add_argument('-sim_seq', action="store", default = 'n/p', dest = 'sim_seq')
-parser.add_argument('-read_s_par', action="store",default = 'n/p', dest = 'read_s_par')
-parser.add_argument('-read_f_par', action="store",default = 'n/p', dest = 'read_f_par')
-parser.add_argument('-read_r_par', action="store",default = 'n/p', dest = 'read_r_par')
+parser.add_argument ("-snp_analysis_type", action="store", default = "n/p",dest ="snp_analysis_type", choices=set(("par","f2wt")))
+parser.add_argument('-read_s_control', action="store",default = 'n/p', dest = 'read_s_control')
+parser.add_argument('-read_f_control', action="store",default = 'n/p', dest = 'read_f_control')
+parser.add_argument('-read_r_control', action="store",default = 'n/p', dest = 'read_r_control')
 parser.add_argument('-cross_type', action="store",default = 'n/p', dest = 'cross_type', choices=set(('oc','bc')))
-parser.add_argument('-is_ref_strain', action="store",default = 'n/p', dest = 'is_ref_strain', choices=set(('ref','noref'))) #We could give the chance not to be provided in F2 sequenced cases
-parser.add_argument('-parental_used_as_control', action="store",default = 'n/p', dest = 'parental_used_as_control', choices=set(('mut','nomut'))) #We could give the possibility not to provide (usage of 2 F2 or reference as parental)
+parser.add_argument('-is_ref_strain', action="store",default = 'n/p', dest = 'is_ref_strain', choices=set(('ref','noref'))) 
+parser.add_argument('-parental_used_as_control', action="store",default = 'n/p', dest = 'parental_used_as_control', choices=set(('mut','nomut')))
 
+
+#[21] $snp_analysis_type [par/f2wt]
 args = parser.parse_args()
 
 project_name = args.project_name
@@ -42,9 +45,10 @@ ann_file = args.ann_file
 sim_mut = args.sim_mut
 sim_recsel = args.sim_recsel
 sim_seq = args.sim_seq
-read_s_par = args.read_s_par
-read_f_par = args.read_f_par
-read_r_par = args.read_r_par
+snp_analysis_type = args.snp_analysis_type #Which control is being used: parental one or F2wt?
+read_s_control = args.read_s_control
+read_f_control = args.read_f_control
+read_r_control = args.read_r_control
 cross_type = args.cross_type
 is_ref_strain = args.is_ref_strain
 parental_used_as_control = args.parental_used_as_control
@@ -85,12 +89,23 @@ if ann_file != "n/p":
 
 #In the analysis mode of snp, cross_type, is_ref_strain and parental_used_as_control are required parameters
 if workflow=="snp":
-	if cross_type == "n/p" or is_ref_strain== "n/p" or parental_used_as_control == "n/p":
-		error = 1
-		problems.append("In order to use the mode snp, values for cross_type (oc/bc), is_ref_strain (ref/noref) or parental_used_as_control (mut/nomut) must be given. See more info in the documentation page")
-	if cross_type == "oc" and is_ref_strain == "noref" and parental_used_as_control == "nomut":
-		error = 1
-		problems.append("Unfortunatelly this software is not thought to allow the process of an outcross which is not in the reference background and the sequenced parental is not the pre-mutagenized one")
+	if snp_analysis_type == "f2wt":
+		if cross_type == "oc":
+			error = 1
+			problems.append("This program do not support an experimental design in which an outcross is performed and the wild type F2 is used as control. Please see documentation for more details.")
+		else:
+			cross_type = "bc"
+			parental_used_as_control = "n/p"
+			if is_ref_strain == "n/p":
+				error = 1
+				problems.append("In order to perform a snp mode using f2wt control analysis it is necessary to specify whether your samples are in a reference background or not. ")
+	if snp_analysis_type == "par"
+		if cross_type == "n/p" or is_ref_strain== "n/p" or parental_used_as_control == "n/p":
+			error = 1
+			problems.append("In order to use the mode snp, values for cross_type (oc/bc), is_ref_strain (ref/noref) or parental_used_as_control (mut/nomut) must be given. See more info in the documentation page")
+		if cross_type == "oc" and is_ref_strain == "noref" and parental_used_as_control == "nomut":
+			error = 1
+			problems.append("Unfortunatelly this software is not thought to allow the process of an outcross which is not in the reference background and the sequenced parental is not the pre-mutagenized one")
 			
 #If the analysis mode is insertions, a insertions sequence must be given
 if workflow == "ins":
@@ -104,9 +119,9 @@ if workflow == "ins":
     
 if data_source == "sim":
 	if workflow == "snp": 
-		if sim_mut == "n/p" or sim_recsel == "n/p" or sim_seq == "n/p":
+		if sim_mut == "n/p" or sim_recsel == "n/p" or sim_seq == "n/p" or snp_analysis_type == "n/p":
 			error = 1
-			problems.append("In order to perform the simulation mode (sim) for snp (snp) parameters sim_mut, sim_recsel and sim_seq must be given, please find the information regarding then in the software documentation")
+			problems.append("In order to perform the simulation mode (sim) for snp (snp) parameters sim_mut, sim_recsel and sim_seq must be given together with the snp_analysis_type (control used), please find the information regarding then in the software documentation")
 	else:
 		if sim_mut == "n/p" or sim_seq == "n/p":
 			error = 1
@@ -181,32 +196,32 @@ if data_source == "exp":
 			problems.append("Please introduce the files containing the forward reads in " + input_folder)
 		if read_r not in input_folder:
 			error = 1
-			problems.append("Please introduce the files containing the reverse reads in " + input_folder)
+			problems.append("Please introduce the files containing reverse reads in " + input_folder)
 		if read_f == read_r:
 			error = 1
-			problems.append("Please notice both read files have the same name")
+			problems.append("Please notice both  forward and reverse reads files have the same name")
 	if lib_type == "se":
 		if read_s not in input_folder:
 			error = 1
 			problems.append("Please introduce the file containing the reads in easymap/project/0_input directory")	
 
 
-	#For snp mode, parental reads should be provided for the further procesing. ---> it is possible we will not require parental genotyping, in that case here, give the reference as parenta
+	#For snp mode, control reads should be provided for the further procesing. 
 	if workflow == "snp":
 		if lib_type == "pe":
-			if read_f_par not in input_folder:
+			if read_f_control not in input_folder:
 				error = 1
-				problems.append("Please introduce the files containing the forward reads in " + input_folder)
-			if read_r_par not in input_folder:
+				problems.append("Please introduce the files containing the control forward reads in " + input_folder)
+			if read_r_control not in input_folder:
 				error = 1
-				problems.append("Please introduce the files containing the reverse reads in " + input_folder)
-			if read_f_par == read_r_par:
+				problems.append("Please introduce the files containing the control reverse reads in " + input_folder)
+			if read_f_control == read_r_control:
 				error = 1
-			problems.append("Please notice both parental read files have the same name")
+			problems.append("Please notice both control read files have the same name")
 		if lib_type == "se":
 			if read_s not in input_folder:
 				error = 1
-				problems.append("Please introduce the file containing the reads in " + input_folder)	
+				problems.append("Please introduce the file containing the control reads in " + input_folder)	
 
 
 
@@ -218,7 +233,7 @@ if error == 1:
 	quit()	
 
 
-master_program_input = project_name + " " + workflow + " " + data_source + " "+ lib_type + " " + ref_seqs + " " + ins_seq + " " + read_s + " " + read_f + " " + read_r + " " + gff_file + " " + ann_file + " " + sim_mut + " " + sim_recsel + " " + sim_seq + " " + read_s_par + " " + read_f_par + " " + read_r_par + " " + is_ref_strain+ " "+ cross_type+ " "+ parental_used_as_control 
+master_program_input = project_name + " " + workflow + " " + data_source + " "+ lib_type + " " + ref_seqs + " " + ins_seq + " " + read_s + " " + read_f + " " + read_r + " " + gff_file + " " + ann_file + " " + sim_mut + " " + sim_recsel + " " + sim_seq + " " + read_s_control + " " + read_f_control + " " + read_r_control + " " + is_ref_strain+ " "+ cross_type+ " "+ parental_used_as_control+ " "+ snp_analysis_type 
 
 print master_program_input
 print "Succes!"
