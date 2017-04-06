461b795524c34afe34834d11b43a8488b386c949
sergioSEa <sergio_andreu@hotmail.es>
2017-04-06 09:44:41 +0200

    updated version

--- easymap.sh
+++ easymap.sh
@@ -73,7 +73,7 @@
 
 
 # Simulate SNP MbS with F2mt F2wt mode using only chr1
-# ./easymap.sh project-name snp sim se genome.fa n/p n/p n/p n/p chr1.gff n/p 150+e "0,14;1,31;2,33;3,15;4,5;5,2"+1,10000000+r+50 25+200,40+0,0+1+100 n/p n/p n/p oc ref mut f2wt
+# ./easymap.sh project-name snp sim se genome.fa n/p n/p n/p n/p chr1.gff n/p 150+e "0,14;1,31;2,33;3,15;4,5;5,2"+1,10000000+r+50 25+200,40+0,0+1+100 n/p n/p n/p oc ref mut f2wt se
 
 
 
