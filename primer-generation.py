e665276018405138e0605506d73dcddf654d53eb
sergioSEa <sergio_andreu@hotmail.es>
2017-04-26 13:11:55 +0200

    new way

--- primer-generation.py
+++ primer-generation.py
@@ -27,17 +27,17 @@ def Tm_calculation(oligo):
 	"AG": [-7.8, -21.0],
 	"AT": [-7.2,-20.4],	
 	"CA": [-8.5,-22.7],
-    "CC": [-8.0, -19.9],
-    "CG": [-10.6,-27.2],
-    "CT": [-7.8,-21.0],
-    "GA": [-8.2,-22.2],
-    "GC": [-9.8, -24.4],
-    "GG": [-8.0, -19.9],
-    "GT": [-8.4, -22.4],
-    "TA": [-7.2,-21.3],
-    "TC": [-8.2,-22.2],
-    "TG": [-8.5,-22.7],
-    "TT": [-7.9,-22.2]}
+	"CC": [-8.0, -19.9],
+	"CG": [-10.6,-27.2],
+	"CT": [-7.8,-21.0],
+	"GA": [-8.2,-22.2],
+	"GC": [-9.8, -24.4],
+	"GG": [-8.0, -19.9],
+	"GT": [-8.4, -22.4],
+	"TA": [-7.2,-21.3],
+	"TC": [-8.2,-22.2],
+	"TG": [-8.5,-22.7],
+	"TT": [-7.9,-22.2]}
 
 	#Effect on entropy by salt correction; von Ahsen et al 1999
 	#Increase of stability due to presence of Mg
@@ -66,21 +66,6 @@ def Tm_calculation(oligo):
    	tm=((1000*h)/(s+(1.987*math.log(primer/2000000000))))-273.15
    	return tm
 
-def obtain_values(File):
-	compilation= set()
-	ordered_list = []
-	positions = open(File,"r")
-	n = 0
-	for line in positions.readlines():
-		line = line.split("\t")
-		if line[5] != "nh" and n != 0:
-			data = (line[1], line[2])
-			ordered_list.append(data)
-			mode = line[0]
-			compilation.add(data)
-		n += 1
-	compilation = sorted(compilation, key=lambda x: ordered_list.index(x)) #sets doesn't keep the order, this function sorts the set as it was made in the list, set is necessary in order to remove the redundant mutations
-	return compilation,mode
 
 def reverse_complementary(oligo):
 	revcomp = oligo.translate(maketrans('ACGT', 'TGCA'))[::-1]
@@ -100,22 +85,17 @@ def read_fasta(fp):
 	if name: yield (name, ''.join(seq))
 
 
-inputt= obtain_values(args.File)
-datos = inputt[0]
-mode = inputt[1]
-data = []
-for items in datos:
-	data.append(items)
 
-
-with open(genome) as fp:
-	for name_contig, seq_contig in read_fasta(fp):
-		if name_contig[1:].lower() == data[0][0]:
-			genome = seq_contig
+def genome_selection(contig,genome):
+	with open(genome) as fp:
+		for name_contig, seq_contig in read_fasta(fp):
+			if name_contig[1:].lower() == contig:
+				genome = seq_contig
+		return genome
 
 def rule_1(oligo,location):
 	last_element = len(oligo)
-	if location == "downstream" : oligo == reverse_complementary(oligo)
+	if location == "downstream" : oligo = reverse_complementary(oligo)
 	while True:
 		end_of_primer = 21
 		begin_of_primer = 0
@@ -171,51 +151,76 @@ def rule_2(oligo,location):
 		begin_of_primer +=1
 
 
-#Total size of the amplification 
-size_i = 300
-size_f = 500
-if mode == "snp":
-	dic = {}
-	dic_Tm= {}
-	for snp in data:
-		dic[snp[1]] = []
-		dic_Tm[snp[1]] = []
-		#upstream primer
-		up_primer_pos = int(snp[1]) - size_i
-		oligo = genome[up_primer_pos-1 : up_primer_pos + 100]
-		result = rule_1(oligo, "upstream")
+def snp_calculation(position,genome):
+	#Total size of the amplification 
+	size_i = 300
+	size_f = 500
+
+	oligos = []
+	Tms = []
+	#upstream primer
+	up_primer_pos = int(position) - size_i
+	oligo = genome[up_primer_pos-1 : up_primer_pos + 100]
+	result = rule_1(oligo, "upstream")
+	if result[0] == "no":
+		result = rule_2(oligo, "upstream")
 		if result[0] == "no":
-			result = rule_2(oligo, "upstream")
+			oligos.append("not found")
+			oligos.append("-")
+			Tms.append("-")
+			Tms.append("-")
+	if result[0] == "yes":
+		oligos.append(result[1])
+		Tms.append(str(result[2])) 
+		#downstream primer
+		down_primer_pos = int(position) + size_f
+		oligo = genome[down_primer_pos-1 : down_primer_pos + 100]
+		result = rule_1(oligo,"downstream")
+		if result[0] == "no":
+			result = rule_2(oligo, "downstream")
 			if result[0] == "no":
-				dic[snp[1]].append("not found")
-				dic[snp[1]].append("-")
-				dic_Tm[snp[1]].append("-")
-				dic_Tm[snp[1]].append("-")
+				oligos.append("not found")
+				Tms.append("-")
 		if result[0] == "yes":
-			dic[snp[1]].append(result[1])
-			dic_Tm[snp[1]].append(str(result[2]))
-			#downstream primer
-			down_primer_pos = int(snp[1]) + size_f
-			oligo = genome[down_primer_pos-1 : down_primer_pos + 100]
-			result = rule_1(oligo,"downstream")
-			if result[0] == "no":
-				result = rule_2(oligo, "downstream")
-				if result[0] == "no":
-					dic[snp[1]].append("not found")
-					dic_Tm[snp[1]].append("-")
-			if result[0] == "yes":
-				dic[snp[1]].append(result[1])
-				dic_Tm[snp[1]].append(str(result[2]))
-
-result = open("primers_file", "w")
-m = 0
-for snp in data:
-	if m == 0:
-		result.write("Mode\tchromosome\tposition\tforward primer\tTm forward\treverse primer\tTm reverse\n")
-	result.write(mode+"\t"+snp[0]+"\t"+snp[1]+"\t"+dic[snp[1]][0]+"\t"+dic_Tm[snp[1]][0]+"\t"+dic[snp[1]][1]+ "\t"+dic_Tm[snp[1]][1]+"\n")
-	m +=1
-	print snp[1], dic[snp[1]]
-print m
-		
-
+			oligos.append(result[1])
+			Tms.append(str(result[2]))
+	return oligos,Tms
 	
+			
+
+
+positions = open(args.File,"r")
+result = open("variants2.txt","w")
+result.write("@type\tcontig\tposition\tref_base\talt_base\thit\tmrna_start\tmrna_end\tstrand\tgene_model\tgene_element\taa_pos\taa_ref\taa_alt\tdistance_to_selected_position\tforward primer\tTm forward\treverse primer\tTm reverse\n")
+n = 0
+first_list = []
+for line in positions.readlines():
+	line = line.split("\t")
+	if n != 0:
+		first_list.append(line)	
+	n += 1
+
+former = ""
+list2= []
+contig_used = ""
+
+for line in first_list:
+	v = line[0]+"\t"+line[1]+"\t"+line[2]+"\t"+line[3]+"\t"+line[4]+"\t"+line[5]+"\t"+line[6]+"\t"+line[7]+"\t"+line[8]+"\t"+line[9]+"\t"+line[10]+"\t"+line[11]+"\t"+line[12]+"\t"+line[13]+"\t"+line[14].rstrip()
+	if line[5] == "nh":
+		list2.append(v+"\t-\t-\t-\t-\n")
+	else:
+		a = line[1]+"-"+line[2]
+		mode = line[0]
+		if a == former:
+			list2.append(v+"\t-\t-\t-\t-\n")
+		elif a != former:
+			if mode == "snp":
+				if line[1] != contig_used:
+					genom = genome_selection(line[1],genome)
+					contig_used = line[1]
+				r = snp_calculation(line[2],genom)
+				list2.append(v+"\t"+r[0][0]+"\t"+r[1][0]+"\t"+r[0][1]+"\t"+r[1][1]+"\n")
+		former = a
+
+for items in list2:
+	result.write(items)
