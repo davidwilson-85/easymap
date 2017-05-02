979edc649c0d9b1ed9723c40d0231208f7a16a51
sergioSEa <sergio_andreu@hotmail.es>
2017-05-02 14:07:43 +0200

    Se

--- run.sh
+++ run.sh
@@ -5,26 +5,25 @@ directory=./PRIMERS/
 #Loop through all files in the directory and DO the SAM to BAM conversion and the genereation of consensus sequence from each insertion consensus file. 
 for i in $directory* 
 do
-	echo $i
     if test -f "$i" 
     then
     #SAM to BAM
+    substring=${i%.*}
        {
-			./samtools1/samtools view $i > alignment.bam 
+			./samtools1/samtools sort $i  > $substring.bam 
 
 		}
 
-		exit
-
 		{
-			./samtools1/samtools mpileup -uf pbinprok2.fa alignment.bam | ./bcftools-1.3.1/bcftools call -c | ./bcftools-1.3.1/vcfutils.pl vcf2fq > cns.fq
+			./samtools1/samtools mpileup -uf pbinprok2.fa $substring.bam | ./bcftools-1.3.1/bcftools call -c | ./bcftools-1.3.1/vcfutils.pl vcf2fq > cns.fq
 		} || {
 			echo $(date) ': Error ' 
 
 		}
 		echo $(date) ': finished.'
+		sed "s/'@pbinprok/@$substring/" ./cns.fq
     fi
    #Concatenate all the fastaq files into one big fq file, which will be given as an input for the primer generation script
-    cat cns.fq > all_insetions_cns.fq
+	cat cns.fq >> all_insetions_cns.fq
 done
 
