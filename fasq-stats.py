ad8025810b7260715ad89330427433a5b0f11c62
sergioSEa <sergio_andreu@hotmail.es>
2017-05-16 12:56:52 +0200

    stats ready

--- fasq-stats.py
+++ fasq-stats.py
@@ -7,12 +7,8 @@ parser.add_argument('-fasq', action="store", dest = 'File', required = "True")
 
 args = parser.parse_args()
 reverse = "no"
-files = args.File.split(",")
-if len(files) == 2:
-	forward = files[0]
-	reverse = files[1]
-if len(files) == 1:
-	forward = files[0]
+files = args.File
+
 
 #phred_not="J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,[,\,],^,_,`,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,{,|,},~"
 
@@ -20,45 +16,47 @@ def character_to_ASCII(string):
 	st = []
 	for items in string:
 		ascii = ord(items)
-		if ascii > 73: ascii = ascii-64
-		else: 	ascii = ascii-33
-		st.append(str(ascii))
+		ascii = ascii-33
+		st.append(int(ascii))
 	return st
 
-
-def fasq_process(fil):
-	fastaq_dic = {}
+def fasq_preprocess(fil):
+	fastaq_list = []
 	i = 0
 	with open(fil) as fastaq:
-		n = 0
+		p = 1
 		for lines in fastaq:
-			n += 1
 			lines = lines.rstrip()
-			if lines.startswith("@"):
-				pos = lines
-				fastaq_dic[pos]= ""
-				m = 0
-			elif lines.startswith("+"):
-				m = 1
-				continue
-			elif m == 1:
-				fastaq_dic[pos]+=lines
-				i += 1
-			if i >= 50000:
+			if p%4 == 0:
+				fastaq_list.append(lines)
+			i += 1
+			p +=1
+			if i > 20000:
 				break
-
-		#number_reads = float(n)/4
-		#if number_reads < 1000000:
-		#	representative_amount = number_reads
-		#else:
-		#	representative_amount = 1000000
+		return fastaq_list
+def fasq_process(fil,x,group):
+	dic = {}
+	dic[str(x)+"_"+str(x+group)] = []
+	i = 0
+	p = 1
+	with open(fil) as fastaq:
+		for lines in fastaq:
+			lines = lines.rstrip()
+			if p%4== 0:
+				for items in range(x,x+group):
+					#print x, x+group,items
+					try:
+						dic[str(x)+"_"+str(x+group)].append(lines[items])
+					except:
+						h = "h"
+				i += 1
+			
+			if i >= 500000:
+				break
+			p += 1
 	qual_dic= {}
-	n = 0	
-	for position in fastaq_dic:
-		qual_dic[position] = character_to_ASCII(fastaq_dic[position])
-		n += 1
-		#if n >= representative_amount:
-		#	break
+	for position in dic:
+		qual_dic[position] = character_to_ASCII(dic[position])
 	return qual_dic
 def average(lista):
 	n = 0 
@@ -66,12 +64,14 @@ def average(lista):
 	for items in lista:
 		l.append(float(items))
 		n += 1
-	average = sum(l)/n
+	try:
+		average = sum(l)/n
+	except:
+		average = 0
 	return average 
 def boxplot_stats(lista):
 	lista = sorted(lista)
 	lenght = len(lista)
-	
 	position1 = float(lenght/2)
 	if position1.is_integer():
 		per_50 = lista[int(position1)]
@@ -107,50 +107,36 @@ def boxplot_stats(lista):
 	IQR = int(per_75) - int(per_25)
 	Extreme_positive = 1.5*IQR + float(per_75)
 	Extreme_negative =  float(per_25)-1.5*IQR 
+	
 	if Extreme_negative < 0:
 		Extreme_negative = 0
-	if float(min(lista))> Extreme_negative: Extreme_negative = min(lista)
-	if float(max(lista))< Extreme_positive: Extreme_positive = max(lista)
 	
-	return Extreme_negative, per_25, per_50, per_75, Extreme_positive
+	if float(min(lista))> float(Extreme_negative): Extreme_negative = min(lista)
+	
+	if float(max(lista))< float(Extreme_positive): Extreme_positive = max(lista)
+	
+	
+	return float(Extreme_negative), float(per_25), float(per_50), float(per_75), float(Extreme_positive)
 
-def calculations(dic_pos):
+def calculations(dic_pos): 	
+	for item in dic_pos:
+		average_val = average(dic_pos[item])
+		stats = boxplot_stats(dic_pos[item])
+
+	return average_val,stats[0],stats[1],stats[2],stats[3],stats[4]
+
+def lenght_reads_cal(pre_dic):
 	value_list = []
 	biggest = 0
-	for reads in dic_pos:	
-		if len(dic_pos[reads]) > biggest:
-			biggest = len(dic_pos[reads]) 
-
-	dic_qual =  {}		
-	for position in range(biggest):
-		dic_qual[position] = []
-		for reads in dic_pos:
-			dic_qual[position].append(dic_pos[reads][position])
-			
-	dic_pos.clear()
-	dic_final = {}
-	m = 0 
-	for values in dic_qual:
-		if m == 0:
-			val = []	
-		val.extend(dic_qual[values])
-		m += 1
-		if m == 5:
-			intervalo = int(values) - m
-			intervalo = str(intervalo)+"-"+str(values)
-			if intervalo not in dic_final: dic_final[intervalo] = []
-			dic_final[intervalo].append(average(val))
-			stats = boxplot_stats(val)
-			
-			dic_final[intervalo].extend([stats[0],stats[1],stats[2],stats[3],stats[4]])
-			m = 0
-
-	return dic_final 
-
+	for reads in pre_dic:	
+		if len(reads) > biggest:
+			biggest = len(reads) 
+	return biggest
 
 def Draw_box_plot(table):
-	fnt1 = ImageFont.truetype('easymap-server/fonts/arial.ttf', 7)
+	fnt1 = ImageFont.truetype('easymap-server/fonts/VeraMono.ttf', 7)
 	#Size of the window
+	print table
 	a = 20
 	b = 20
 	c = 20
@@ -165,7 +151,9 @@ def Draw_box_plot(table):
 	draw.line(((a, c) + (a, position_y_exe)), fill=(0, 0, 0, 0), width=1)
 	size_x_exe = len(table)*10 +10 #number of positions*10 pxls which is one will take + 10 for the position 1. 
 	draw.line(((a, position_y_exe) + (a+size_x_exe, position_y_exe)), fill=(0, 0, 0, 0), width=1) 
-	
+	#Close chart
+	draw.line(((a, c) + (size_x_exe+a, c)), fill=(0, 0, 0, 0), width=1)
+	draw.line(((size_x_exe+a, c) + (size_x_exe+a, position_y_exe)), fill=(0, 0, 0, 0), width=1)
 	#Vertical values
 	step = float(size_y_exe)/42
 	for values in range(42,-1,-1):
@@ -174,25 +162,31 @@ def Draw_box_plot(table):
 		#draw.text((a-8,20+values*step) + (a-8,20+values*step), pos, font=fnt1, fill=(0,0,0,0))
 		if values%5 == 0:
 			draw.line(((a,20+abs(values-42)*step) + (a-5,20+abs(values-42)*step)), fill=(0, 0, 0, 0), width=1)
+			##draw.text((a-8,20+values*step) + (a-10,20+values*step), str(values-42), font=fnt1, fill=(0,0,0,0))
 
 	i = 10 + a #indentation + space for the first box (same space as in size_x_exe)
 	for position in table:
+		name = position[0]
+		position = position[1:]
+		
+
 		#write the position in the x axe
 		draw.line(((i, position_y_exe) + (i, position_y_exe+5)), fill=(0, 0, 0, 0), width=1)
+		##draw.text((i, position_y_exe+8) + (i,position_y_exe+8), name, font=fnt1, fill=(0,0,0,0))
 
 		#Create a line from the begining to the end of the parameters
-		beg = float(table[position][1]) * step
-		end = float(table[position][-1]) * step
+		beg = float(position[1]) * step
+		end = float(position[-1]) * step
 		draw.line(((i, position_y_exe-beg) + (i, position_y_exe-end)), fill=(0, 0, 0, 0), width=1)
 
 		#Create the boxplot using CORREGIR!!!!!!!!!!
-		beg = float(table[position][2]) * step
-		end = float(table[position][-2]) * step
+		beg = float(position[2]) * step
+		end = float(position[-2]) * step
 		draw.rectangle([(i-3, position_y_exe-beg), (i+3, position_y_exe-end)], fill=(24, 56, 214), outline= None)
 
 		#Draw the average and the MEDIANA?
-		av = float(table[position][0]) * step
-		med = float(table[position][3]) * step
+		av = float(position[0]) * step
+		med = float(position[3]) * step
 		draw.line(((i-3, position_y_exe-med) + (i+3, position_y_exe-med)), fill=(191, 17, 54), width=1)
 		draw.line(((i, position_y_exe-av) + (i, position_y_exe-av)), fill=(50, 214, 25), width=1)
 		i +=10
@@ -204,13 +198,32 @@ def Draw_box_plot(table):
 
 
 
+pre_dic = fasq_preprocess(files)
+lenght_reads = int(lenght_reads_cal(pre_dic))
+
+if lenght_reads<80: group = 1
+if 100>=lenght_reads>80: group = 2
+if lenght_reads>100: group = 5
 
 
-forward_reads = fasq_process(forward)
-forward_table = calculations(forward_reads)
-reverse_reads = "no"
-if reverse != "no": reverse_reads = fasq_process(reverse); reverse_table = calculations(reverse_reads)
+x =0
+final_dic= {}
+#print range((lenght_reads/group)-1)
+#exit()
+final_list=[]
 
-Draw_box_plot(forward_table)
+for repetitions in range((lenght_reads/group)-1):
+	print repetitions
+	lis = []
+	position_table=fasq_process(files,x,group)
+	result = calculations(position_table)
+	lis.append(str(x+1)+"-"+str(x+group))
+	lis.extend(result)
+	final_list.append(lis)
+	
+	#final_dic[str(x+1)+"-"+str(x+group)]= result
+	x += group
+
+Draw_box_plot(final_list)
 
 
