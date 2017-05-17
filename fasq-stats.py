5995a65bc076a9e238ead33f670a48af2d12ac1d
sergioSEa <sergio_andreu@hotmail.es>
2017-05-17 12:38:33 +0200

    finished se

--- fasq-stats.py
+++ fasq-stats.py
@@ -2,6 +2,7 @@
 import argparse
 from PIL import Image, ImageDraw, ImageFont, ImageOps
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument('-fasq', action="store", dest = 'File', required = "True")
 
@@ -51,7 +52,7 @@ def fasq_process(fil,x,group):
 						h = "h"
 				i += 1
 			
-			if i >= 500000:
+			if i >= 5000:
 				break
 			p += 1
 	qual_dic= {}
@@ -134,19 +135,19 @@ def lenght_reads_cal(pre_dic):
 	return biggest
 
 def Draw_box_plot(table):
-	fnt1 = ImageFont.truetype('easymap-server/fonts/VeraMono.ttf', 7)
+	fnt1 = ImageFont.truetype('easymap-server/fonts/VeraMono.ttf', 14)
 	#Size of the window
-	print table
-	a = 20
-	b = 20
+
+	a = 60
+	b = 70
 	c = 20
 	x_window = len(table)*10+ 10+ a + b 
-	y_window = 400
+	y_window = 460
 	#Generation of the file
 	im = Image.new("RGB", (x_window, y_window), (255,255,255))	
 	draw = ImageDraw.Draw(im)
 	#Creation of the axes: exes will start with an indentation of 20 above, below and beside. Each Phred quality score will be in a X% proportion of the y_window  px
-	size_y_exe = y_window-40 #Total size minus above and below indentations
+	size_y_exe = y_window-80 #Total size minus above and below indentations
 	position_y_exe= size_y_exe+20
 	draw.line(((a, c) + (a, position_y_exe)), fill=(0, 0, 0, 0), width=1)
 	size_x_exe = len(table)*10 +10 #number of positions*10 pxls which is one will take + 10 for the position 1. 
@@ -154,26 +155,38 @@ def Draw_box_plot(table):
 	#Close chart
 	draw.line(((a, c) + (size_x_exe+a, c)), fill=(0, 0, 0, 0), width=1)
 	draw.line(((size_x_exe+a, c) + (size_x_exe+a, position_y_exe)), fill=(0, 0, 0, 0), width=1)
+	
 	#Vertical values
 	step = float(size_y_exe)/42
+	j = 0
 	for values in range(42,-1,-1):
 		#pos = str(values+1)
 		draw.line(((a,20+abs(values-42)*step) + (a-4,20+abs(values-42)*step)), fill=(0, 0, 0, 0), width=1)
 		#draw.text((a-8,20+values*step) + (a-8,20+values*step), pos, font=fnt1, fill=(0,0,0,0))
 		if values%5 == 0:
-			draw.line(((a,20+abs(values-42)*step) + (a-5,20+abs(values-42)*step)), fill=(0, 0, 0, 0), width=1)
-			##draw.text((a-8,20+values*step) + (a-10,20+values*step), str(values-42), font=fnt1, fill=(0,0,0,0))
-
+	
+			draw.line(((a,20+abs(values-42)*step) + (a-6,20+abs(values-42)*step)), fill=(0, 0, 0, 0), width=1)
+			text = abs(values-42)
+			w, h = draw.textsize(str(text))
+			draw.text((a-25,20+text*step-h/2-3), str(values), font=fnt1, fill=(0,0,0,0))
+		j +=1	
+			
 	i = 10 + a #indentation + space for the first box (same space as in size_x_exe)
 	for position in table:
 		name = position[0]
 		position = position[1:]
-		
 
 		#write the position in the x axe
-		draw.line(((i, position_y_exe) + (i, position_y_exe+5)), fill=(0, 0, 0, 0), width=1)
-		##draw.text((i, position_y_exe+8) + (i,position_y_exe+8), name, font=fnt1, fill=(0,0,0,0))
+		draw.line(((i, position_y_exe) + (i, position_y_exe+4)), fill=(0, 0, 0, 0), width=1)
 
+		if (i-a)%50 == 0 :
+			draw.line(((i, position_y_exe) + (i, position_y_exe+6)), fill=(0, 0, 0, 0), width=1)
+			w, h = draw.textsize(str(name))
+			if len(name)==3:draw.text((i-w/2-4+1 , position_y_exe+10), name, font=fnt1, fill=(0,0,0,0))
+			if len(name) == 4: draw.text((i-w/2-3 +4, position_y_exe+10), name, font=fnt1, fill=(0,0,0,0))				
+			elif len(name) == 5: draw.text((i-w/2-4, position_y_exe+10), name, font=fnt1, fill=(0,0,0,0))
+			elif len(name) == 6: draw.text((i-w/2-9, position_y_exe+10), name, font=fnt1, fill=(0,0,0,0))
+			elif len(name) == 7: draw.text((i-w/2-14, position_y_exe+10), name, font=fnt1, fill=(0,0,0,0))
 		#Create a line from the begining to the end of the parameters
 		beg = float(position[1]) * step
 		end = float(position[-1]) * step
@@ -191,11 +204,26 @@ def Draw_box_plot(table):
 		draw.line(((i, position_y_exe-av) + (i, position_y_exe-av)), fill=(50, 214, 25), width=1)
 		i +=10
 
+	#Axes draw:
+	x_name = "Position(bp)"
+	draw.text((size_x_exe/2, position_y_exe+35), x_name , font=fnt1, fill=(0,0,0,0)) #Horizontal
+	y_name ="Quality(phred)"
 
+
+
+
+
+
+	label = Image.new("RGB", (140, 20), (255,255,255))
+	draw2 = ImageDraw.Draw(label)
+	draw2.text((1, 1), "Quality(phred)", font=fnt1, fill=(0,0,0))
+
+	label = label.rotate(90)
+	im.paste(label, (2,size_y_exe/2-50))
+
+	#Vertical
 	#save image, specifying the format with the extension
 	im.save("result.png")
-
-
 
 
 pre_dic = fasq_preprocess(files)
@@ -213,7 +241,6 @@ final_dic= {}
 final_list=[]
 
 for repetitions in range((lenght_reads/group)-1):
-	print repetitions
 	lis = []
 	position_table=fasq_process(files,x,group)
 	result = calculations(position_table)
