4bc9529d720ef1b1bcff43c0e72d0cb80ff0b7a7
sergioSEa <sergio_andreu@hotmail.es>
2017-06-16 09:08:03 +0200

    changes

--- workflows/workflow-snp.sh
+++ workflows/workflow-snp.sh
@@ -673,7 +673,7 @@ then
 	# (6) Run mapping analysis
 	my_analysis_mode=out
 	{
-		python $location/scripts_snp/analysis/map-mutation.py -fichero $f1/F2_control_comparison_mapping.va -fasta $f1/$my_gs -mode $my_analysis_mode -window_size 250000 -window_space 25000 -output $f1/map_info.txt -control_modality $my_mutbackgroud -interval_width 4000000 -snp_analysis_type $snp_analysis_type  
+		python $location/scripts_snp/analysis/map-mutation.py -fichero $f1/F2_control_comparison_mapping.va -fasta $f1/$my_gs -mode $my_analysis_mode -window_size 250000 -window_space 25000 -output $f1/map_info.txt -control_modality noref -interval_width 4000000 -snp_analysis_type $snp_analysis_type  
 
 	} || {
 		echo $(date)': Error during execution of map-mutation.py .' >> $my_log_file
