729c8c01a39b2f7ca44ff5f8f4332a89dc2f4bd2
samuellup <slup@umh.es>
2017-11-08 12:56:32 +0100

    -

--- tests_snp_fast.sh
+++ tests_snp_fast.sh
@@ -1,4 +1,22 @@
 
+echo CASO 2___________________________________________________________________________________________
+./easymap -n minicaso2_lou -w snp -sim -r mid -g complete.gff -ed ref_bc_f2wt  -a TAIR10_gene_info.txt --low-stringency
+#rm -rf ./user_projects/*/1_intermediate_files/sim_data/
+
+
+echo CASO 2___________________________________________________________________________________________
+./easymap -n minicaso2_hai -w snp -sim -r mid -g complete.gff -ed ref_bc_f2wt  -a TAIR10_gene_info.txt
+#rm -rf ./user_projects/*/1_intermediate_files/sim_data/
+
+
+
+
+
+
+
+
+exit
+
 echo CASO 4___________________________________________________________________________________________
 ./easymap -n minicaso4 -w snp -sim -r insim -g complete.gff -ed ref_oc_parpol -a TAIR10_gene_info.txt
 #rm -rf ./user_projects/*/1_intermediate_files/sim_data/
